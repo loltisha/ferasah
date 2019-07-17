42544393aceb07a8c5be539cf0d388421b4d0574
loltisha <alyahadiem@gmail.com>
2019-07-17 23:02:14 +0300

    Updates

---
+++ precache-manifest.d1d2a572d12143e0482ce40b355a7e0f.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7b5573871a8fe7e29d5ee686edc7fc7",
+    "url": "/ferasah/index.html"
+  },
+  {
+    "revision": "cd202cb218813ad3ae56",
+    "url": "/ferasah/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "de99ee0a14eae06a3ec5",
+    "url": "/ferasah/static/css/main.5fe8c2e9.chunk.css"
+  },
+  {
+    "revision": "cd202cb218813ad3ae56",
+    "url": "/ferasah/static/js/2.6304fb9d.chunk.js"
+  },
+  {
+    "revision": "de99ee0a14eae06a3ec5",
+    "url": "/ferasah/static/js/main.bf30b801.chunk.js"
+  },
+  {
+    "revision": "9638c6d204c339a5077e",
+    "url": "/ferasah/static/js/runtime~main.4f175ca4.js"
+  },
+  {
+    "revision": "386c40d996ef9b1dfc110f3229ca4319",
+    "url": "/ferasah/static/media/02050.386c40d9.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ferasah/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0867cefc2057ec6e0ff1406d3a73f7b8",
+    "url": "/ferasah/static/media/logof.0867cefc.jpg"
+  },
+  {
+    "revision": "fef23d6b7bd104deed49d58f10dc17f3",
+    "url": "/ferasah/static/media/sa.fef23d6b.jpg"
+  },
+  {
+    "revision": "03d2c79b45e07dc6dee5918b5e4306e4",
+    "url": "/ferasah/static/media/wq.03d2c79b.jpg"
+  },
+  {
+    "revision": "57f22d2384db5e15babffa0f5ce077eb",
+    "url": "/ferasah/static/media/xccd.57f22d23.jpg"
+  },
+  {
+    "revision": "1f383b1f415cd294d6d47386f08f7030",
+    "url": "/ferasah/static/media/بثب.1f383b1f.jpg"
+  }
+]);
